d31347559d4afda2516e8b3d0dc4db86b298d73d
bf-v <bfv00000@gmail.com>
2018-11-23 23:55:26 -0500

    Updates

---
+++ precache-manifest.1d082c5ddb43dbeb179f70dd52816998.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "43a2f772776f0dc55f02",
+    "url": "/bfv/static/js/runtime~main.43a2f772.js"
+  },
+  {
+    "revision": "d91a7c9145d8a340e727",
+    "url": "/bfv/static/js/main.d91a7c91.chunk.js"
+  },
+  {
+    "revision": "726cb51e5e4550bfc03d",
+    "url": "/bfv/static/js/1.726cb51e.chunk.js"
+  },
+  {
+    "revision": "d91a7c9145d8a340e727",
+    "url": "/bfv/static/css/main.ca6e3d2e.chunk.css"
+  },
+  {
+    "revision": "2c68c88b76342af1d7afe83fbf3342f1",
+    "url": "/bfv/index.html"
+  }
+];
