b5c4ed203ab628853bf885e51f700b19bf0e9d7b
Real Lost <thingone656@gmail.com>
2018-11-27 07:31:11 -0500

    mobile-friendly

--- src/Viewer.js
+++ src/Viewer.js
@@ -2,28 +2,41 @@ import React, { Component } from "react";
 import Show from "./Show";
 import "./Viewer.css";
 import _ from "lodash";
-import { ButtonDropdown, DropdownToggle, DropdownMenu, DropdownItem } from "reactstrap";
-import { FaCaretLeft, FaCaretRight } from "react-icons/fa";
+import { ButtonDropdown, DropdownToggle, DropdownMenu, DropdownItem, ButtonGroup } from "reactstrap";
+import { FaCaretLeft, FaCaretRight, FaRandom, FaStarOfLife } from "react-icons/fa";
 
 export default class Viewer extends Component {
   constructor(props) {
     super(props);
     
-    const { files, types} = this.props 
+    const { files, types} = this.props;
+
+    //only need to be generated once
+    let dropItemsLocal = [];
+    for (let key in types) {
+      dropItemsLocal.push(<DropdownItem key={key} onClick={this.select}>{types[key]}</DropdownItem>)
+    }
+
     this.state = {
       currentType: types[0],
       q: _.shuffle(files[types[0]]),
       pos: 0,
-      dropdown: false
+      dropdown: false,
+      WindowWidth: window.innerWidth,
+      isMobile: window.innerWidth <= 600,
+      dropItems: dropItemsLocal,
+      ButtonIconSize: 40 
     };
   }
 
   componentDidMount(){
-    window.addEventListener('keydown', this.handleKeys)
+    window.addEventListener('keydown', this.handleKeys);
+    window.addEventListener('resize', this.handleWindowSizeChange);
   }
 
   componentWillUnmount() {
-    window.removeEventListener('keydown', this.handleKeys)
+    window.removeEventListener('keydown', this.handleKeys);
+    window.removeEventListener('resize', this.handleWindowSizeChange);
   }
 
   handleKeys = e => {
@@ -67,34 +80,80 @@ export default class Viewer extends Component {
         q: _.shuffle(this.props.files[selected]),
         pos: 0
     })
-}
+  }
+
+  handleWindowSizeChange = () => {
+    this.setState({ 
+      WindowWidth: window.innerWidth,
+      isMobile : this.state.WindowWidth <= 600
+    });
+  }
+
+  renderButtonDropDown = () => {
+    const isMobile = this.state.isMobile;
+    const { q, pos, dropdown, currentType, dropItems } = this.state;
+    const { types } =  this.props;
+
+    if(isMobile){//mobile style
+    return (<ButtonDropdown isOpen={dropdown} toggle={this.toggle}>
+      <DropdownToggle color="black">
+        <FaStarOfLife size={this.state.ButtonIconSize} color='#f8f9fa'/>
+      </DropdownToggle>
+      <DropdownMenu>
+        {dropItems}
+      </DropdownMenu>
+    </ButtonDropdown>)
+    }else{ //desktop style
+    return (<ButtonDropdown isOpen={dropdown} toggle={this.toggle}>
+      <DropdownToggle caret color="black">
+        {currentType}
+      </DropdownToggle>
+      <DropdownMenu>
+        {dropItems}
+      </DropdownMenu>
+    </ButtonDropdown>)
+    }
+  }
+
+  renderButtonBar = () => {
+    const { q, pos, dropdown, currentType, isMobile } = this.state;
+    if(isMobile){
+      return <ButtonGroup vertical block className="button-container">
+        <button disabled={pos === 0} onClick={this.prev}>
+          <FaCaretLeft size={this.state.ButtonIconSize} color='#f8f9fa'/>
+        </button>
+        <button onClick={this.shuffle}> 
+          { isMobile ? <FaRandom size={this.state.ButtonIconSize} color='#f8f9fa'/>:"Shuffle" }
+        </button>
+
+        { this.renderButtonDropDown() }
+        <button disabled={pos === q.length - 1} onClick={this.next}>
+          <FaCaretRight size={this.state.ButtonIconSize} color='#f8f9fa'/>
+        </button>
+      </ButtonGroup>
+    }else{
+      return <ButtonGroup className="button-container">
+        <button disabled={pos === 0} onClick={this.prev}>
+          <FaCaretLeft size={this.state.ButtonIconSize} color='#f8f9fa'/>
+        </button>
+        <button onClick={this.shuffle}> 
+          { isMobile ? <FaRandom size={this.state.ButtonIconSize} color='#f8f9fa'/>:"Shuffle" }  
+        </button>
+
+        { this.renderButtonDropDown() }
+        <button disabled={pos === q.length - 1} onClick={this.next}>
+          <FaCaretRight size={this.state.ButtonIconSize} color='#f8f9fa'/>
+        </button>
+      </ButtonGroup>
+    }
+  }
 
   render() {
-    const { q, pos, dropdown, currentType } = this.state;
-    const { types } =  this.props
-    let dropItems = []
-    for (let key in types) {
-      dropItems.push(<DropdownItem key={key} onClick={this.select}>{types[key]}</DropdownItem>)
-    }
+    const { q, pos, dropdown, currentType, isMobile } = this.state;
+
     return (
       <div className="viewer-container">
-        <div className="button-container">
-          <button disabled={pos === 0} onClick={this.prev}>
-            <FaCaretLeft size={40} color='#f8f9fa'/>
-          </button>
-          <button onClick={this.shuffle}>Shuffle</button>
-          <ButtonDropdown isOpen={dropdown} toggle={this.toggle}>
-            <DropdownToggle caret color="black">
-              {currentType}
-            </DropdownToggle>
-            <DropdownMenu>
-              {dropItems}
-            </DropdownMenu>
-          </ButtonDropdown>
-          <button disabled={pos === q.length - 1} onClick={this.next}>
-            <FaCaretRight size={40} color='#f8f9fa'/>
-          </button>
-        </div>
+        { this.renderButtonBar() }
         <div className="item-container">
           <Show key={q[pos]} link={q[pos]} />
         </div>
