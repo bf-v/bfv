be361c832b34563430964428011b82367fb0d52d
bf-c <45364989+bf-c@users.noreply.github.com>
2018-11-28 19:10:01 +0530

    Move shuffling/FileReader logic to FileInput

--- src/App.js
+++ src/App.js
@@ -2,39 +2,11 @@ import React, { Component } from "react";
 import FileInput from "./FileInput";
 import Viewer from "./Viewer";
 
-function shuffled(inp) {
-  const arr = inp.slice();
-  for (let i = arr.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [arr[i], arr[j]] = [arr[j], arr[i]];
-  }
-  return arr;
-}
-
 export default class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { content: null, shuffle: true };
-    this.fr = new FileReader();
-    this.fr.addEventListener("loadend", () => {
-      const csv = this.fr.result.split("\n");
-      if (csv[0] !== "listname,source,type,description") {
-        window.alert("Provided csv file is not a BF export");
-        return;
-      }
-      // links is a list of (listname, source) tuples
-      let content = csv
-        .slice(1)
-        .filter(x => !!x)
-        .map(r => r.split(",").slice(0, 2));
-      content = this.state.shuffle ? shuffled(content) : content;
-      this.setState({ content });
-    });
-  }
+  state = { content: null };
 
-  handleChange = (evt, shuffle) => {
-    this.setState({ shuffle });
-    this.fr.readAsText(evt.target.files[0]);
+  handleChange = content => {
+    this.setState({ content });
   };
 
   render() {
--- src/FileInput.js
+++ src/FileInput.js
@@ -1,8 +1,39 @@
 import React, { Component } from "react";
 import "./FileInput.css";
 
+function shuffled(inp) {
+  const arr = inp.slice();
+  for (let i = arr.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [arr[i], arr[j]] = [arr[j], arr[i]];
+  }
+  return arr;
+}
+
 export default class FileInput extends Component {
-  state = { shuffle: true };
+  constructor(props) {
+    super(props);
+    this.state = { shuffle: true };
+    this.fr = new FileReader();
+    this.fr.addEventListener("loadend", () => {
+      const csv = this.fr.result.split("\n");
+      if (csv[0] !== "listname,source,type,description") {
+        window.alert("Provided csv file is not a BF export");
+        return;
+      }
+      // links is a list of (listname, source) tuples
+      let content = csv
+        .slice(1)
+        .filter(x => !!x)
+        .map(r => r.split(",").slice(0, 2));
+      content = this.state.shuffle ? shuffled(content) : content;
+      this.props.onChange(content);
+    });
+  }
+
+  handleChange = evt => {
+    this.fr.readAsText(evt.target.files[0]);
+  };
 
   render() {
     return (
@@ -31,7 +62,7 @@ export default class FileInput extends Component {
             id="file-input"
             type="file"
             accept=".csv"
-            onChange={e => this.props.onChange(e, this.state.shuffle)}
+            onChange={this.handleChange}
           />
           <label id="choose-file-label" htmlFor="file-input">
             Choose a file
