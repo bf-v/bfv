e4e9c61a199a31f9e1001a41d7d7478d194ed161
bf-c <45364989+bf-c@users.noreply.github.com>
2022-09-06 00:02:39 +0530

    redgifs fixes

--- src/show/gfycat/GfycatStore.js
+++ src/show/gfycat/GfycatStore.js
@@ -3,7 +3,11 @@ const getUrlFromRedGifs = async _gfyId => {
   const resp = await fetch(`https://api.redgifs.com/v2/gifs/${gfyId}`);
   const json = await resp.json();
   const { urls } = json.gif;
-  return urls.hd || urls.sd;
+  const url = new URL(urls.hd || urls.sd);
+  if (url.hostname === 'thumbs2.redgifs.com') {
+    url.hostname = 'thumbs3.redgifs.com';
+  }
+  return url.toString();
 };
 
 const getUrlFromGfycat = async gfyId => {
@@ -28,12 +32,12 @@ const resolveFirstSequentially = async promises => {
   throw new AggregateError(errors);
 };
 
-const gfycatStoreKey = "gfycatUrls";
+const gfycatStoreKey = 'gfycatUrls';
 
 const GfycatStore = (() => {
   let store;
   try {
-    store = JSON.parse(localStorage.getItem(gfycatStoreKey) || "{}");
+    store = JSON.parse(localStorage.getItem(gfycatStoreKey) || '{}');
   } catch (e) {
     store = {};
   }
@@ -51,19 +55,19 @@ const GfycatStore = (() => {
       if (!(gfyId in store)) {
         const url = await resolveFirstSequentially([
           () => getUrlFromRedGifs(gfyId),
-          () => getUrlFromGfycat(gfyId)
+          () => getUrlFromGfycat(gfyId),
         ]);
         if (!url) {
-          throw new Error("No URL found for Gfycat");
+          throw new Error('No URL found for Gfycat');
         }
         store[gfyId] = {
           url,
-          expiresAt: new Date().getTime() + 1 * 24 * 60 * 60 * 1000 // 1 day
+          expiresAt: new Date().getTime() + 1 * 24 * 60 * 60 * 1000, // 1 day
         };
       }
       localStorage.setItem(gfycatStoreKey, JSON.stringify(store));
       return store[gfyId].url;
-    }
+    },
   };
 })();
 
