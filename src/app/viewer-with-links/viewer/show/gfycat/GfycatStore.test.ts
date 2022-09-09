dd61a909c906773d1826c19902fa437c11a4df74
bf-c <45364989+bf-c@users.noreply.github.com>
2022-09-10 00:01:29 +0530

    Add tests

---
+++ src/app/viewer-with-links/viewer/show/gfycat/GfycatStore.test.ts
@@ -0,0 +1,23 @@
+import { describe, expect, it } from 'vitest';
+import { getUrlFromGfycat, getUrlFromRedGifs } from './GfycatStore';
+import axios from 'axios';
+
+describe('getUrlFromRedGifs', () => {
+  it('should work', async () => {
+    const url = await getUrlFromRedGifs('EnchantingScaryConure');
+    const { headers, status } = await axios.head(url);
+    expect(status).toBe(200);
+    expect(headers['content-type']).toStrictEqual('video/mp4');
+  });
+});
+
+describe('getUrlFromGfycat', () => {
+  it('should work', async () => {
+    const url = await getUrlFromGfycat('DrearyBrownClam');
+    const { headers, status } = await axios.head(url);
+    expect(status).toBe(200);
+    expect(headers['content-type']).toStrictEqual('video/mp4');
+  });
+});
+
+export {};
