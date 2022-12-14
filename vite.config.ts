7156c9fd101c3cdd4451e5a0e0856489aa5909f8
bf-c <45364989+bf-c@users.noreply.github.com>
2022-12-14 23:35:22 +0530

    PWA

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,42 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import eslint from 'vite-plugin-eslint';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/bfv/',
-  plugins: [react(), eslint()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        short_name: 'BFV',
+        theme_color: '#000000',
+        background_color: '#ffffff',
+        display: 'standalone',
+        name: 'BFV',
+        icons: [
+          {
+            src: 'favicon.ico',
+            sizes: '64x64 32x32 24x24 16x16',
+            type: 'image/x-icon',
+          },
+          {
+            src: 'logo192.png',
+            type: 'image/png',
+            sizes: '192x192',
+          },
+          {
+            src: 'logo512.png',
+            type: 'image/png',
+            sizes: '512x512',
+          },
+        ],
+      },
+    }),
+    eslint(),
+  ],
   server: {
     port: 3000,
   },
